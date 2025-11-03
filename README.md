2978a50513406f56a6f88c91144a8c04cf377b4b
SoraKumo <info@croud.jp>
2025-11-03 15:17:54 +0900

    first-commit

--- README.md
+++ README.md
@@ -1,36 +1,72 @@
-This is a [Next.js](https://nextjs.org) project bootstrapped with [`create-next-app`](https://nextjs.org/docs/app/api-reference/cli/create-next-app).
+# next-callback-event
 
-## Getting Started
+関数のインスタンスを更新せず、関数の挙動のみ更新するサンプル
 
-First, run the development server:
+```tsx
+"use client";
+import {
+  useEffect,
+  useState,
+  useCallback,
+  useRef,
+  useImperativeHandle,
+} from "react";
 
-```bash
-npm run dev
-# or
-yarn dev
-# or
-pnpm dev
-# or
-bun dev
+// 関数のインスタンスを変えずに、関数の挙動を更新する
+export function useCallbackEvent<T extends (...args: unknown[]) => unknown>(
+  callback: T
+) {
+  const property = useRef<{ callback: T }>(null);
+  useImperativeHandle(property, () => ({ callback }));
+  return useCallback((...args: Parameters<T>) => {
+    if (!property.current) throw "callback is null";
+    return property.current.callback(...args);
+  }, []);
+}
+
+const Page = () => {
+  const [valueA, setValueA] = useState(10);
+  const [valueB, setValueB] = useState(20);
+  const [answer, setAnswer] = useState(0);
+
+  // valueA,valueBを更新しつつ、関数のインスタンスは変えない
+  const handleAnswer = useCallbackEvent(() => {
+    setAnswer(valueA + valueB);
+  });
+
+  // handleAnswerのインスタンスは変更されないので、useEffectのコールバックkはvalueA,valueBを更新しても呼ばれない
+  useEffect(() => {
+    const t = setInterval(() => {
+      handleAnswer();
+    }, 100);
+    console.log("effect");
+    return () => clearInterval(t);
+  }, [handleAnswer]);
+
+  return (
+    <div className="p-4">
+      <div>
+        A:
+        <input
+          className="outline p-1"
+          type="number"
+          value={valueA}
+          onChange={(e) => setValueA(Number(e.target.value))}
+        />
+      </div>
+      <div>
+        B:
+        <input
+          className="outline p-1"
+          type="number"
+          value={valueB}
+          onChange={(e) => setValueB(Number(e.target.value))}
+        />
+      </div>
+      <div>Answer:{answer}</div>
+    </div>
+  );
+};
+
+export default Page;
 ```
-
-Open [http://localhost:3000](http://localhost:3000) with your browser to see the result.
-
-You can start editing the page by modifying `app/page.tsx`. The page auto-updates as you edit the file.
-
-This project uses [`next/font`](https://nextjs.org/docs/app/building-your-application/optimizing/fonts) to automatically optimize and load [Geist](https://vercel.com/font), a new font family for Vercel.
-
-## Learn More
-
-To learn more about Next.js, take a look at the following resources:
-
-- [Next.js Documentation](https://nextjs.org/docs) - learn about Next.js features and API.
-- [Learn Next.js](https://nextjs.org/learn) - an interactive Next.js tutorial.
-
-You can check out [the Next.js GitHub repository](https://github.com/vercel/next.js) - your feedback and contributions are welcome!
-
-## Deploy on Vercel
-
-The easiest way to deploy your Next.js app is to use the [Vercel Platform](https://vercel.com/new?utm_medium=default-template&filter=next.js&utm_source=create-next-app&utm_campaign=create-next-app-readme) from the creators of Next.js.
-
-Check out our [Next.js deployment documentation](https://nextjs.org/docs/app/building-your-application/deploying) for more details.
--- app/page.tsx
+++ app/page.tsx
@@ -1,65 +1,66 @@
-import Image from "next/image";
+"use client";
+import {
+  useEffect,
+  useState,
+  useCallback,
+  useRef,
+  useImperativeHandle,
+} from "react";
 
-export default function Home() {
+// 関数のインスタンスを変えずに、関数の挙動を更新する
+export function useCallbackEvent<T extends (...args: unknown[]) => unknown>(
+  callback: T
+) {
+  const property = useRef<{ callback: T }>(null);
+  useImperativeHandle(property, () => ({ callback }));
+  return useCallback((...args: Parameters<T>) => {
+    if (!property.current) throw "callback is null";
+    return property.current.callback(...args);
+  }, []);
+}
+
+const Page = () => {
+  const [valueA, setValueA] = useState(10);
+  const [valueB, setValueB] = useState(20);
+  const [answer, setAnswer] = useState(0);
+
+  // valueA,valueBを更新しつつ、関数のインスタンスは変えない
+  const handleAnswer = useCallbackEvent(() => {
+    setAnswer(valueA + valueB);
+  });
+
+  // handleAnswerのインスタンスは変更されないので、useEffectのコールバックkはvalueA,valueBを更新しても呼ばれない
+  useEffect(() => {
+    const t = setInterval(() => {
+      handleAnswer();
+    }, 1000);
+    console.log("effect");
+    return () => clearInterval(t);
+  }, [handleAnswer]);
+
   return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
+    <div className="p-4">
+      <div>
+        A:
+        <input
+          className="outline p-1"
+          type="number"
+          value={valueA}
+          onChange={(e) => setValueA(Number(e.target.value))}
         />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the page.tsx file.
-          </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
-          </p>
-        </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
-            />
-            Deploy Now
-          </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
-        </div>
-      </main>
+      </div>
+      <div>
+        B:
+        <input
+          className="outline p-1"
+          type="number"
+          value={valueB}
+          onChange={(e) => setValueB(Number(e.target.value))}
+        />
+      </div>
+      <div>Answer:{answer}</div>
     </div>
   );
-}
+};
+
+export default Page;
